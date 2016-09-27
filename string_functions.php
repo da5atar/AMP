7f458dd017ea64afe4223f4dfd94454f28250e93
Massamba Sow <mass.sow@gmail.com>
2016-09-27 11:09:21 +0000

    Looked at some basic string functions

--- string_functions.php
+++ string_functions.php
@@ -22,15 +22,15 @@
 	Uppercase first: <?php echo ucfirst($third); ?><br />
 	Uppercase words: <?php echo ucwords($third); ?><br />
 	<br />
-	<!-- Length: <?php echo strlen($third); ?><br />
-	Trim: <?php echo "A" . trim(" B C D ") . "E"; ?><br />
-	Find: <?php echo strstr($third, "brown"); ?><br />
-	Replace by string: <?php echo str_replace("quick", "super-fast", $third); ?><br />
+	Length: <?php echo strlen($third); ?><br /> <!-- strlen function returns the string length -->
+	Trim: <?php echo "A" . trim(" B C D ") . "E"; ?><br /> <!-- trim function removes the leading/trailing whitespaces -->
+	Find: <?php echo strstr($third, "brown"); ?><br /> <!-- strstr function finds a string within a string -->
+	Replace by string: <?php echo str_replace("quick", "super-fast", $third); ?><br /> <!-- str_replace function replaces a string within a string , here search the haystack ($third) for the needle (quick) to be replaced by "super-fast"  -->
 	<br />
 	Repeat: <?php echo str_repeat($third, 2); ?><br />
-	Make substring: <?php echo substr($third, 5, 10); ?><br />
+	Make substring: <?php echo substr($third, 5, 10); ?><br /> <!-- substr function returns a sub string from a string. Here returns substr from $third starting at the 5th char position to the 10th position -->
 	Find position: <?php echo strpos($third, "brown"); ?><br />
 	Find character: <?php echo strchr($third, "z"); ?><br />
-	-->
+	
 	</body>
 </html>
